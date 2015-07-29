caa5c7325d948b23d586db74e9d0e0b3ed8c2c72
'Uladox <Uladox@users.noreply.github.com>
2015-07-28 22:05:41 -1000

    Changes from GPL3 to LGPL3

--- othm_symbols.c
+++ othm_symbols.c
@@ -1,3 +1,18 @@
+/*    This file is part of Othm_symbols.
+ *
+ *    Othm_symbols is free software: you can redistribute it and/or modify
+ *    it under the terms of the Lesser GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Othm_symbols is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Lesser GNU General Public License for more details.
+ *
+ *    You should have received a copy of the Lesser GNU General Public License
+ *    along with Ruspma.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "othm_symbols.h"
 #include <string.h>
 #include <stdio.h>
